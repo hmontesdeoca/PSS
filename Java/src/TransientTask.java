169f7ff784d399e69c9b4614c1bb5e8cd88cbfb1
Howard <montes.de.oca.howard@gmail.com>
2021-05-05 15:10:20 -0700

    added end time to transient tasks

--- Java/src/TransientTask.java
+++ Java/src/TransientTask.java
@@ -1,9 +1,18 @@
 public class TransientTask extends Task {
     private int date;
+    private float endTime;
 
-    public TransientTask(String name, String type, float startTime, float duration, int date) {
+    public TransientTask(String name, String type, float startTime, float duration,  int date) {
         super(name, type, startTime, duration);
         this.date = date;
+        float temp = startTime + duration;
+        if(temp > 24) {
+            temp = temp - 24;
+            endTime = temp;
+        }
+        else{
+            endTime=temp;
+        }
     }
 
     public int getDate() {
@@ -12,5 +21,9 @@ public class TransientTask extends Task {
 
     public void setDate(int date) {
         this.date = date;
+    }
+
+    public float getEndTime(){
+        return endTime;
     }
 }
