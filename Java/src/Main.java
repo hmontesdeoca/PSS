2207a53f1aeabc96721e5fb7862389608f9ef212
Howard <montes.de.oca.howard@gmail.com>
2021-05-17 17:51:33 -0700

    added more GUIS

---
+++ Java/src/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/main.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ Java/src/TaskController.java
@@ -0,0 +1,19 @@
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+
+public class TaskController {
+
+    @FXML
+    private Button addTask = new Button();
+
+    //No Args constructor required
+    public TaskController(){
+
+    }
+    public void changeColor(){
+
+    }
+}
